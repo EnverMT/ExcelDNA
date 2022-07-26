4aad203f98138dbd44b258e2b5e36456ebd9c004
EnverMT <anvarmtg@gmail.com>
2022-07-26 15:49:37 +0500

    Add project files.

---
+++ Planning Tools/RibbonController.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using ExcelDna.Integration.CustomUI;
+
+namespace Ribbon
+{
+    [ComVisible(true)]
+    public class RibbonController : ExcelRibbon
+    {
+        public void OnButtonPressed_Cells_MyCopy(IRibbonControl control)
+        {
+            MessageBox.Show("Not yet implemented " + control.Id);
+        }
+        public void OnButtonPressed_Cells_Paste(IRibbonControl control)
+        {
+            MessageBox.Show("Not yet implemented " + control.Id);
+        }
+        public void OnButtonPressed_Cells_Zero(IRibbonControl control, string itemID, int itemIndex)
+        {
+            MessageBox.Show($"Not yet implemented: control={control} \n idemID={itemID}'\n itemIndex={itemIndex}");
+        }
+        public void OnButtonPressed_Cells_NullString(IRibbonControl control)
+        {
+            MessageBox.Show("Not yet implemented ");
+        }
+    }
+}
